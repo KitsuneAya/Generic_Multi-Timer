4cba4fd59b71ecfb019db0d607a9f3d23243a3c7
KitsuneAya <kaccipiter92@gmail.com>
2024-04-22 00:14:40 -0400

    Added code that will attempt to fix the audio and play it if it fails to play the first time.

--- src/app/Audio.java
+++ src/app/Audio.java
@@ -7,13 +7,14 @@ import java.io.FileNotFoundException;
 
 /**
  * @author <a href="https://steamcommunity.com/id/KitsuneAya/">Ayaya</a>
- * @version 1.1
+ * @version 1.2
  * @apiNote This class is for the instantiation of objects that can play a sound
  */
 public class Audio {
 
     private Player audio;
     private Thread audioThread;
+    private String filePath;
 
     /**
      * Instantiates a blank Audio object for use as a placeholder
@@ -29,6 +30,7 @@ public class Audio {
      */
     public Audio(String filePath) {
 
+        this.filePath = filePath;
         setAudioPlayer(filePath);
 
     }
@@ -54,25 +56,34 @@ public class Audio {
 
         if (this.audio != null) {
 
-            this.audioThread = new Thread(() -> {
-                try {
-                    this.audio.play();
-                } catch (
-                        JavaLayerException e) {
-                    this.audio.close();
-                    throw new RuntimeException(e);
-                }
-            });
+                // This beautiful piece of code will attempt to play the audio
+                // and if not successful, will re-set the Player and try again
+                this.audioThread = new Thread(() -> {
+                    for (int i = 1; i <= 2; i++) {
+                        try {
+                            this.audio.play();
+                            break;
+                        } catch (JavaLayerException e) {
+                            this.audio.close();
+                            this.setAudio(this.filePath);
+                            if (i < 2) {
+                                continue;
+                            }
+                            e.printStackTrace(); // If not successful in the 2nd attempt, print the stack trace
+                        }
+                    }
+                });
 
             this.audioThread.setDaemon(true);
             this.audioThread.start();
+
 
         }
     }
 
     public void stop() {
 
-        if (this.audio != null) {
+        if (this.audioThread != null) {
             this.audio.close();
             this.audioThread.interrupt();
         }
